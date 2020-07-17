93cfabf7f9639e98069b72a8e17a7d83d7b2243c
aravindpanil <aravindpanil@gmail.com>
2020-07-18 02:56:09 +0530

    Create master list from db file

---
+++ read_db.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+default_db = 'C:\ProgramData\GOG.com\Galaxy\storage\galaxy-2.0.db'
+
+
+def open_db():
+    conn = None
+    try:
+        conn = sqlite3.connect(default_db)
+    except Error as e:
+        print(e)
+
+    return conn
+
+
+open_db()
+
+
+def create_master(conn=open_db()):
+    cur = conn.cursor()
+    cur.execute("""SELECT GamePieces.releaseKey, GamePieces.gamePieceTypeId, GamePieces.value FROM GameLinks
+				JOIN GamePieces ON GameLinks.releaseKey = GamePieces.releaseKey;""")
+    return cur.fetchall()
+
+
+var = create_master(open_db())
