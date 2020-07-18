58cdcaab53d582e613929ddcbf95cf3f798eb0a2
aravindpanil <aravindpanil@gmail.com>
2020-07-18 17:27:28 +0530

    Import Tags into MasterDB

--- read_db.py
+++ read_db.py
@@ -13,14 +13,10 @@ def open_db():
     return conn
 
 
-open_db()
-
-
 def create_master(conn=open_db()):
     cur = conn.cursor()
-    cur.execute("""SELECT GamePieces.releaseKey, GamePieces.gamePieceTypeId, GamePieces.value FROM GameLinks
-				JOIN GamePieces ON GameLinks.releaseKey = GamePieces.releaseKey;""")
+    cur.execute("""SELECT GamePieces.releaseKey, GamePieces.gamePieceTypeId, GamePieces.value, UserReleaseTags.tag
+                   FROM GameLinks
+				   LEFT JOIN GamePieces ON GameLinks.releaseKey = GamePieces.releaseKey
+				   LEFT JOIN UserReleaseTags ON GameLinks.releaseKey = UserReleaseTags.releaseKey""")
     return cur.fetchall()
-
-
-var = create_master(open_db())
